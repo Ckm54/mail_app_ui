d195bbbd7c84660fd2c551e2dc607a2058737ec1
collo <mutaic84@gmail.com>
2023-04-20 11:10:53 +0300

    :hammer: upate lib file

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,13 @@
+library widgets;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:material_design/models/models.dart';
+import 'package:material_design/models/data.dart' as data;
+
+part 'email_list_view.dart';
+part 'email_widget.dart';
+part 'email/email_content.dart';
+part 'email/email_headline.dart';
+part 'email/email_reply_options.dart';

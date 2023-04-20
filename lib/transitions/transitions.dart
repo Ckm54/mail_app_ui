a9caecbe2c5acea62860564d3ce939eac573ac36
collo <mutaic84@gmail.com>
2023-04-20 14:23:29 +0300

    :hammer: update transitions

--- lib/transitions/transitions.dart
+++ lib/transitions/transitions.dart
@@ -1,8 +1,11 @@
 library transitions;
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import '../animations/animations.dart';
 
 part 'bottom_bar_transition.dart';
 part 'nav_rail_transition.dart';
+part 'list_detail_transition.dart';

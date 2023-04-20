c5f028b978d7d0a34e6a5a4d6a44e119b3f6177b
collo <mutaic84@gmail.com>
2023-04-20 11:00:12 +0300

    :sparkles: create email list view component

---
+++ lib/widgets/email_list_view.dart
@@ -0,0 +1,41 @@
+part of widgets;
+
+class EmailListView extends StatelessWidget {
+  const EmailListView({
+    super.key,
+    this.selectedIndex,
+    this.onSelected,
+    required this.currentUser,
+  });
+
+  final int? selectedIndex;
+  final ValueChanged<int>? onSelected;
+  final User currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: ListView(
+        children: [
+          const SizedBox(
+            height: 8,
+          ),
+
+          // Search bar
+
+          const SizedBox(
+            height: 8,
+          ),
+          ...List.generate(data.emails.length, (index) {
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+
+              // Email widget as child
+            );
+          })
+        ],
+      ),
+    );
+  }
+}
